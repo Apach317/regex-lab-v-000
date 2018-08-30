98c7a1019d2770a1925bafe0ede634b49d440550
Ron Lynch <lynch.ronald1@gmail.com>
2018-08-30 17:08:52 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -22,8 +22,12 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
   end
 end
 
-
-
 def valid_phone_number?(phone)
-
+  if phone.match(/\d{7}/) then
+    return true
+  else
+    return false
+  end
+  end
 end
+"2438894546", "(718)891-1313", "234 435 9978", "(800)4261134"
