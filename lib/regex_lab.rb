8064ab0c6bcc1aad8f42eece968ce326b0f89968
Ron Lynch <lynch.ronald1@gmail.com>
2018-08-30 16:28:38 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,8 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  text.grep(/^\w{5}\s/) == true
+  if text.grep(/^\w{5}\s/)
+    return true
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
